5f74ef9da9d0e649549a0803c0916856c8ffee72
henwill8 <43818215+henwill8@users.noreply.github.com>
2025-08-31 11:15:50 -0600

    Update config for production deployment
    Switched ENV to 'prod' and updated production API URL in config.js. Modified the web server command in package.json to use '--host 0.0.0.0' for compatibility.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Set this variable to switch between dev and prod
-const ENV = "dev";  // Change this to "prod" when deploying to production
+const ENV = "prod";  // Change this to "prod" when deploying to production
 
 const DEV_API_URL = "http://localhost:8000";  // Development server URL
-const PROD_API_URL = "https://thecarpenterwall.com";  // Production server URL
+const PROD_API_URL = "https://api.thecarpenterwall.com";  // Production server URL
 
 // Determine which API URL to use based on the ENV variable
 const API_URL = ENV === "prod" ? PROD_API_URL : DEV_API_URL;
